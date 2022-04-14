6760d8bd7a96a5b3b95ee9b4e5c71755365d7308
Deepak Vishwakarma <79501955+deepakvishwakarma-hh@users.noreply.github.com>
2022-04-14 15:29:18 +0530

    Delete Header of XS

--- components/elements/header/index.tsx
+++ components/elements/header/index.tsx
@@ -2,22 +2,18 @@
 
 import Logo from "../../micros/Logo";
 import style from "./style.module.scss";
-import MobileResponsiveMenu from "./MobileResponsiveMenu"
 import HireDeveloperButton from "../../micros/HireDeveloperButton";
 
-interface prop {
-    clean?: boolean
-}
 
-const Header = ({ clean }: prop) => (
+const Header = () => (
     <nav className={style.wrapper}>
         <Logo />
 
         <div className={style['btn-grp']}>
-            {!clean && <HireDeveloperButton />}
+            <HireDeveloperButton />
         </div>
 
-        {!clean && <MobileResponsiveMenu />}
+        {/* {!clean && <MobileResponsiveMenu />} */}
 
     </nav >)
 
