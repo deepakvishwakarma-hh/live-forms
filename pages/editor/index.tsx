4e2ad42039b14d50734ddadcb9b6beb05d0a2ff2
Deepak Vishwakarma <79501955+deepakvishwakarma-hh@users.noreply.github.com>
2022-04-17 14:54:16 +0530

    update /editor : responsive as like /dashboard

--- pages/editor/index.tsx
+++ pages/editor/index.tsx
@@ -1,19 +1,21 @@
 
+import Head from "next/head";
 import { useEffect } from "react";
 import * as Elements from "../../components/elements"
 import useSave from "../../components/hooks/useSave";
 import Security from "../../components/elements/auth-boundry";
 
 const _Generator = () => {
-
     const save = useSave();
-
     useEffect(() => {
         save.saveSteps();
     })
 
     return (
         <Security>
+            <Head>
+                <meta name="viewport" content="width" />
+            </Head>
             <Elements.FormGenerator />
         </Security>
     )
