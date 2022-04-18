b25199d77056e25218dae2eed450eea89060c7bd
Deepak Vishwakarma <79501955+deepakvishwakarma-hh@users.noreply.github.com>
2022-04-18 10:51:19 +0530

    Title , shortcut icon added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,25 @@
 import { Container } from "@chakra-ui/react"
 import * as Elements from "../components/elements"
-
+import Head from "next/head"
 const Home = () => {
 
   return (
-    <Container maxW='container.lg' >
-      <Elements.Header />
-      <Elements.Intro />
-      <Elements.Features />
-      <Elements.Footer />
-      <Elements.HomeFloat />
-    </Container>
+
+    <>
+      <Head>
+        <title>Liveforms</title>
+        <link rel="shortcut icon" href="/icons.svg" />
+
+      </Head>
+
+      <Container maxW='container.lg' >
+        <Elements.Header />
+        <Elements.Intro />
+        <Elements.Features />
+        <Elements.Footer />
+        <Elements.HomeFloat />
+      </Container>
+    </>
   )
 }
 
