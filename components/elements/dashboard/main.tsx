20bbf9c89ece560de6c5e41c250541c81b12f5f8
Deepak Vishwakarma <79501955+deepakvishwakarma-hh@users.noreply.github.com>
2022-04-25 09:58:24 +0530

    dashboard/main : new feature

--- components/elements/dashboard/main.tsx
+++ components/elements/dashboard/main.tsx
@@ -1,17 +1,26 @@
 import Form from "./forms";
-import { Grid, Flex } from "@chakra-ui/react";
+import Image from "next/image";
+import Router from "next/router";
+import { Grid, Flex, Center, Button } from "@chakra-ui/react";
 
 const Main = ({ arr, data }: any) => {
 
     const maps = arr.map((item: string, index: number) => <Form data={data[item]} key={index} />)
 
+    const isDataNotFound = data == 'data-not-found'
+
     return (
 
         <Grid gridTemplate={" auto / 100%"} >
 
-            <Flex flexWrap={'wrap'} bg="white" padding={5} h="fit-content">
-                {maps}
-            </Flex>
+            {!isDataNotFound && <Flex flexWrap={'wrap'} bg="white" padding={5} h="fit-content"> {maps}</Flex>}
+
+            {isDataNotFound && <Center >
+                <Flex flexDir={'column'} py={10} borderRadius={3} px={3} >
+                    <Image src="/content.svg" width={400} height={400} alt="hii" />
+                    <Button onClick={() => { Router.push('/editor') }}>Create New Survey</Button>
+                </Flex>
+            </Center>}
 
         </Grid>
     )
