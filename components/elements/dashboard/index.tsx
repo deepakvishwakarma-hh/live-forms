4100ff7f437ec993eda5d83c79f5e661e9f5c241
Deepak Vishwakarma <79501955+deepakvishwakarma-hh@users.noreply.github.com>
2022-04-25 09:01:15 +0530

    remove  unused code

--- components/elements/dashboard/index.tsx
+++ components/elements/dashboard/index.tsx
@@ -5,15 +5,12 @@ import Loader from "../loader";
 
 const Dashboard = ({ arr, data }: any) => {
 
-    console.log(data)
     return (
         <Grid gridTemplate={'100vh / 100px auto'}>
             <Aside />
-
-
-
-
-            {(data) ? <Main arr={arr} data={data} /> : <Loader />}
+            {(data) ?
+                <Main arr={arr} data={data} />
+                : <Loader />}
         </Grid>
     )
 };
