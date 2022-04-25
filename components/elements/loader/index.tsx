abba14cb34f60751ceacb4151225a22f5554c7bd
Deepak Vishwakarma <79501955+deepakvishwakarma-hh@users.noreply.github.com>
2022-04-25 09:06:21 +0530

    up Loader component : remove auto Refresh  functionality

--- components/elements/loader/index.tsx
+++ components/elements/loader/index.tsx
@@ -1,16 +1,10 @@
 interface prop {
     fullpage?: boolean
 }
-import { useEffect } from "react";
 import { motion } from "framer-motion"
 import { Center, Box } from "@chakra-ui/react";
-import { useRouter } from "next/router";
 
 const Loader = ({ fullpage }: prop) => {
-
-    const router = useRouter()
-
-
 
     const Spinner = motion(Box)
 
@@ -23,21 +17,6 @@ const Loader = ({ fullpage }: prop) => {
             }
         },
     }
-
-    useEffect(() => {
-        const Intervel = setInterval(() => {
-            console.log('refreshed')
-            const path = router.asPath;
-            router.replace(path)
-
-        }, 3000)
-
-
-        return () => {
-            clearInterval(Intervel)
-        }
-    }, [])
-
 
     return (
         <Center pos={(fullpage) ? 'fixed' : "static"} top={0} left={0} w="100%" h="100%" bg="white" zIndex={999}>
