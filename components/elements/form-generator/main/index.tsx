25ca1954752ed86dfa198c7153c5695f2bbb8711
Deepak Vishwakarma <79501955+deepakvishwakarma-hh@users.noreply.github.com>
2022-04-26 21:09:17 +0530

    update Dashboard's ~ main ~ top

--- components/elements/form-generator/main/index.tsx
+++ components/elements/form-generator/main/index.tsx
@@ -23,23 +23,21 @@ const Main = () => {
             bgImage="url('/dashboard-bg.jpg')"
         >
 
-            <Flex px={5} bg="white"
-                alignItems={'center'} >
+            <Flex px={5} bg="white" alignItems={'center'} >
 
                 <Flex alignItems={'center'} >
 
-                    <Tooltip label=" survey heading">
+                    <Tooltip label="survey heading">
                         <Button onClick={onFormHeaderClick} bg="none">
                             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 16 16">
                                 <path d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h13zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z" />
                                 <path d="M3 8.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5zm0-5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5v-1z" />
                             </svg>
+                            <Text pl={2} fontSize={12}>Title & Paragraph</Text>
                         </Button>
                     </Tooltip>
 
                 </Flex>
-
-                <Text fontSize={13} ml={'auto'}>{META.__header.title}</Text>
 
                 <Box marginLeft={'auto'}>
                     <Button _hover={{ opacity: .8 }} fontSize={13} letterSpacing={1} bg='black' color={'white'} p={'1rem 2rem'} fontWeight={400} onClick={onProduce} > Produce</Button>
