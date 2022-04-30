3959d8e13b5bc624eab8e4dc6b8172ef5695ab04
Deepak Vishwakarma <79501955+deepakvishwakarma-hh@users.noreply.github.com>
2022-04-30 12:20:47 +0530

    update survey pages comps

--- components/elements/survey/index.tsx
+++ components/elements/survey/index.tsx
@@ -17,23 +17,24 @@ const Survey = ({ Client, Creator }: prop) => {
     const { title, subtitle } = Client.__header;
 
     return (
-        <Container maxW="container.lg">
+        <Container maxW="container.lg" h="100vh" p="0">
 
-            <Flex py={5} flexDir="column">
-                <Heading py={5} fontWeight={500} fontSize={25} as="h1" textAlign="center">{title}</Heading>
-                <Text fontSize={15} color="grey" textAlign="center">{subtitle}</Text>
-
+            <Flex py={10} flexDir="column" bg="whiteAlpha.500">
+                <Heading py={5} fontWeight={300} fontSize={35} as="h1" textAlign="center">{title} </Heading>
+                <Text fontSize={12} color="black" textAlign="center">🔗{subtitle}</Text>
             </Flex>
 
-            <Flex justifyContent={'center'} bg="gray.200" h={'300px'} borderRadius={5} pt={5}>
+            <Flex justifyContent={'center'} borderRadius={5} pt={5}>
                 < Transformer live>{Client}</Transformer>
             </Flex>
 
-            <Flex ml="auto" flexDir={'column'} bg="whatsapp.200" p={5} pos="fixed" bottom={'2rem'} right={'2rem'}>
+            {/* <Flex ml="auto" flexDir={'column'} bg="whatsapp.200" p={5} pos="fixed" bottom={'2rem'} right={'2rem'}>
                 <Text textTransform={'uppercase'} fontWeight={500} fontSize={13}> Publisher</Text>
                 <Text fontWeight={500} fontSize={12} color="purple">{displayName}</Text>
-                <Text fontWeight={500} fontSize={12} color="purple">{email}</Text>
-            </Flex>
+                <Text fontWeight={500} fontSize={12} color="purple">{email}</Text>  
+            </Flex> */}
+
+
 
         </Container >
 
