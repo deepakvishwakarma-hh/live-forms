4ea7fdde9c60e2793c2e66129e167b0d76a5bc05
Deepak Vishwakarma <79501955+deepakvishwakarma-hh@users.noreply.github.com>
2022-04-30 12:31:33 +0530

    update /survey : head SEO

--- pages/survey/[id].tsx
+++ pages/survey/[id].tsx
@@ -1,10 +1,10 @@
+import Head from 'next/head';
 import { useRouter } from 'next/router';
 import { useState, useEffect } from 'react'
 import database from "../../firebase.config"
 import { Survey } from "../../components/elements";
 import { Loader } from "../../components/elements";
 import { ref, get, child } from "firebase/database";
-
 // imported under relatview
 import Fallback from "../../components/elements/survey/fallback";
 
@@ -34,10 +34,15 @@ const Page = () => {
             })
     }, [id])
 
-    console.log(res)
+    const pageTitle = res ? res.Client.__header.title : 'loading...'
+    const pageDiscription = res ? res.Client.__header.discription : 'loading...'
 
     return (
         <>
+            <Head>
+                <title>{pageTitle} ~ by liveforms</title>
+                <meta name="description" content={pageDiscription} />
+            </Head>
             {isLoading && <Loader fullpage />}
             {res && <Survey Client={res.Client} Creator={res.Creator} />}
             {!res && <Fallback />}
