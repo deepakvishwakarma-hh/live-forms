dcff2960b3e99b1edab4969415e0bb528b49791d
Deepak Vishwakarma <79501955+deepakvishwakarma-hh@users.noreply.github.com>
2022-05-01 09:14:50 +0530

    update /survey : self advertise  by forms

--- components/elements/survey/index.tsx
+++ components/elements/survey/index.tsx
@@ -9,13 +9,13 @@ interface prop {
 import { Client } from "../../typos";
 import { Transformer } from "../index";
 import { Heading, Flex, Text, Center } from "@chakra-ui/react"
-
+import Router from "next/router";
 
 const Survey = ({ Client, Creator }: prop) => {
 
     const { displayName, email } = Creator
     const { title, subtitle } = Client.__header;
-
+    const onLogoClickHandler = () => { Router.push('/') }
 
     return (
 
@@ -31,7 +31,22 @@ const Survey = ({ Client, Creator }: prop) => {
                 alignItems={'start'}
                 flexDir="column">
 
-                <Text maxW={'800px'} fontSize={15} color="purple">@{displayName}</Text>
+
+
+                <Flex alignItems={'center'} >
+
+                    <Text maxW={'800px'} fontSize={15} color="black" mr={2}>Powered by</Text>
+
+                    <Flex cursor={'pointer'} onClick={onLogoClickHandler} border="2px whitesmoke solid" borderRadius={10} pr={3} alignItems={'center'} bg="whitesmoke">
+                        <svg width="80" height="30" viewBox="0 0 100 50" >
+                            <rect width="100" height="50" fill="white" />
+                            <rect x="22" y="14" width="56" height="7" rx="3.5" fill="black" />
+                            <rect x="22.5" y="25.5" width="55" height="6" rx="3" fill="transparent" stroke="black" />
+                        </svg>
+                        <Text fontWeight={500}>Liveforms</Text>
+                    </Flex>
+                </Flex>
+
                 <Heading maxW={'500px'} py={5} fontWeight={700} fontSize={35} as="h1">{title} </Heading>
                 <Text maxW={'800px'} fontSize={15} color="grey">{subtitle}</Text>
 
