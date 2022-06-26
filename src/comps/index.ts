2fb90c8de7e26af67e80c9a10da57166503550c2
Deepak Vishwakarma <79501955+deepakvishwakarma-hh@users.noreply.github.com>
2022-06-26 15:31:38 +0530

    Rise of New Showcase  (discriptive)
    delete old /showcase, update index page, need to composition in future.

--- src/comps/index.ts
+++ src/comps/index.ts
@@ -1,13 +1,9 @@
-import Intro from "./home-intro";
 import Editor from "./editor";
 import Loader from "./loader";
 import Survey from "./survey";
-import Header from "./home-header";
-import Features from "./home-features";
 import Analyser from "./analyser";
 import Dashboard from "./dashboard";
-import Footer from "./home-footer/inder";
 import HomeFloat from "./home-floating-btn"
 import Boundry from "./auth-boundry";
 import Transformer from "./transformer";
-export { Header, Footer, Intro, Features, Transformer, Dashboard, Boundry, Loader, Survey, HomeFloat, Analyser, Editor };
+export { Transformer, Dashboard, Boundry, Loader, Survey, HomeFloat, Analyser, Editor };
