7b87b1a3709662e396c499cf37089781161dd5e1
Deepak Vishwakarma <79501955+deepakvishwakarma-hh@users.noreply.github.com>
2022-06-26 16:12:06 +0530

    CS : code structured

--- src/comps/auth-boundry/loginError.tsx
+++ src/comps/auth-boundry/loginError.tsx
@@ -1,14 +1,12 @@
+import Image from "next/image";
 import useAuth from "../../hooks/useAuth";
 import { Flex, Center, Text, Button } from "@chakra-ui/react";
-import Image from "next/image";
+
 const Aleart = () => {
-
     const Auth = useAuth();
-
     const onLogin = () => {
         Auth.googleAuth()
     }
-
     return (
         <Center pos="fixed" top="0" left="0" width="100%" height="100%" bg="whiteAlpha.300">
             <Flex minW={'300px'} borderRadius={3} flexDir={'column'} px={5} py={10}>
@@ -16,11 +14,8 @@ const Aleart = () => {
                 <Text textAlign={'center'} py={5}> Login not found.</Text>
                 <Button padding=".5rem auto" variant="unstyled" border="2px solid black" fontSize={13} onClick={onLogin}>Login with Google</Button>
             </Flex>
-
         </Center>
     )
 
 }
 export default Aleart
-
-// undraw_login_re_4vu2.svg
