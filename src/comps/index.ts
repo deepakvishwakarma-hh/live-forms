63caecbd7d154485189a4d516045736595d8b09d
Deepak Vishwakarma <79501955+deepakvishwakarma-hh@users.noreply.github.com>
2022-06-27 10:52:03 +0530

    remove unused component

--- src/comps/index.ts
+++ src/comps/index.ts
@@ -3,7 +3,6 @@ import Loader from "./loader";
 import Survey from "./survey";
 import Analyser from "./analyser";
 import Dashboard from "./dashboard";
-import HomeFloat from "./home-floating-btn"
 import Boundry from "./auth-boundry";
 import Transformer from "./transformer";
-export { Transformer, Dashboard, Boundry, Loader, Survey, HomeFloat, Analyser, Editor };
+export { Transformer, Dashboard, Boundry, Loader, Survey, Analyser, Editor };
